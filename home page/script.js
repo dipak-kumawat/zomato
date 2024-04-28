83324cabcfc765cc8e883453462cf6adb00e60c0
dipak-kumawat <dipaksk11@gmail.com>
2024-04-28 21:30:47 +0530

    adding popup in order section

--- home page/script.js
+++ home page/script.js
@@ -1,4 +1,3 @@
-
 var overlayloader = document.getElementById("loader");
 const overlayLoader = document.getElementById("loader");
 
@@ -8,128 +7,81 @@ window.addEventListener("load", () => {
   }, 1000); // Hides the loader after 4 seconds (4000 milliseconds)
 });
 
+let menu = document.querySelector("#menu-bars");
+let navbar = document.querySelector(".navbar");
 
+menu.onclick = () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+};
 
-let menu = document.querySelector('#menu-bars');
-let navbar = document.querySelector('.navbar');
+menu.onscroll = () => {
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
+};
 
-menu.onclick = () =>{
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-}
+document.querySelector("#search-icon").onclick = () => {
+  document.querySelector("#search-form").classList.toggle("active");
+};
 
-menu.onscroll= () =>{
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
-}
-
-document.querySelector('#search-icon').onclick = () =>{
-    document.querySelector('#search-form').classList.toggle('active');
-}
-
-document.querySelector('#close').onclick = () =>{
-    document.querySelector('#search-form').classList.remove('active');
-}
+document.querySelector("#close").onclick = () => {
+  document.querySelector("#search-form").classList.remove("active");
+};
 
 var swiper = new Swiper(".home-slider", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 7500,
-      disableOnInteraction: false,
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  loop: true,
+});
+
+var swiper = new Swiper(".review-slider", {
+  spaceBetween: 20,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+
+  loop: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
     },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+    640: {
+      slidesPerView: 2,
     },
-
-    loop:true,
-  });
-
-  var swiper = new Swiper(".review-slider", {
-    spaceBetween: 20,
-    centeredSlides: true,
-    autoplay: {
-      delay: 7500,
-      disableOnInteraction: false,
+    786: {
+      slidesPerView: 2,
     },
-
-    loop:true,
-    breakpoints: {
-      0:{
-        slidesPerView: 1,
-      },
-      640:{
-        slidesPerView: 2,
-      },
-      786:{
-        slidesPerView: 2,
-      },
-      1024:{
-        slidesPerView: 3,
-      },
-    }
-
-  });
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
 
 
 
+let popup = document.getElementById("popup");
+let background  = document.getElementById("background");
+
+function openPopup() {
+  popup.classList.add("open-popup");
+  background.classList.add("open-popup");
+}
+
+function closePopup() {
+  popup.classList.remove("open-popup");
+  background.classList.remove("open-popup");
+}
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-  
